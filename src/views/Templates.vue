78d40939519f990eb0219ed708817a55238a4487
Meow_Dev <aljonjimenez091200@gmail.com>
2025-07-25 05:40:13 +0800

    push updated

--- src/views/Templates.vue
+++ src/views/Templates.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { defineComponent } from 'vue'
-
 </script>
 
 <template>
-    <h2>Hello</h2>
+  <div class="mx-auto p-6 w-full">
+    <div class="flex justify-between items-center mb-6">
+      <h1 class="text-2xl font-bold text-gray-800">Templates</h1>
+      <button class="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600">
+        Add Template
+      </button>
+    </div>
+  </div>
 </template>
